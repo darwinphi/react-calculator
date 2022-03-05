f4ca4e9b7a31d7fcbcbfef49a325d800c5cfa1fe
Darwin Manalo <darwinmanalophi@gmail.com>
2022-03-05 15:11:54 +0800

    Resize text when overflows, Fix ipad view

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,3 +1,33 @@
+import { useState, useRef, useEffect, useCallback } from "react";
+
 export const Display = ({ value }) => {
-  return <div className="screen">{value}</div>;
+  const textRef = useRef(null);
+  const screenRef = useRef(null);
+  const [fontSize, setFontSize] = useState(5);
+
+  const handleKeydown = useCallback((e) => {
+    if (textRef.current.offsetWidth > screenRef.current.offsetWidth) {
+      setFontSize((fontSize) => fontSize - 0.7);
+    }
+
+    if (value === 0) {
+      setFontSize(5);
+    }
+  });
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown());
+  }, [handleKeydown]);
+
+  return (
+    <>
+      <div className="screen" ref={screenRef}>
+        <label
+          style={{ fontSize: `${fontSize}rem`, paddingLeft: "1rem" }}
+          ref={textRef}
+        >
+          {value}
+        </label>
+      </div>
+    </>
+  );
 };
